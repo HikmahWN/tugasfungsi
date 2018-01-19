a92fb6825070adfc8b520c3b51dab8a57515f026
HikmahWN <32409194+HikmahWN@users.noreply.github.com>
2018-01-19 13:14:46 +0400

    membuat void mekar dan muda dan isinya

--- bunga.cpp
+++ bunga.cpp
@@ -5,9 +5,11 @@ using namespace std;
 class bunga{
 	public:
 		void layu(string warna);
-		
+		void mekar(int ukuran);
+		void muda(string segar);
 		
 		string jenisbunga;
+		int jumlahkelopak;
 };
 
 int main(){
@@ -17,5 +19,6 @@ int main(){
 	berduri.jenisbunga = "Bunga Mawar";
 	bergerombol.jenisbunga = "Bunga Kertas";
 	berbiji.jenisbunga = "Bunga Matahari";
-	
+
+		
 }
