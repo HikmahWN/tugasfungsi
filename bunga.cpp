882b3f082896ad3b69ad74d090d72c22ccba508c
HikmahWN <32409194+HikmahWN@users.noreply.github.com>
2018-01-19 13:20:24 +0400

    membuat output untuk layu
    menampilkan warna layu pada setiap bunga

--- bunga.cpp
+++ bunga.cpp
@@ -24,6 +24,29 @@ int main(){
 	berduri.jumlahkelopak = 20;
 	bergerombol.jumlahkelopak = 5;
 	berbiji.jumlahkelopak = 25;	
+
+	//layu warna
+	cout << (berduri.jenisbunga);
+	berduri.muda("Merah,Merah Muda dan Putih") ;
+	berduri.layu("kekuningan");
+	cout << "jumlah kelopak : " <<berduri.jumlahkelopak<<endl<<endl;
+	
+	cout << (bergerombol.jenisbunga);
+	bergerombol.muda("Merah Muda dan Putih") ;
+	bergerombol.layu("kecoklatan");
+	cout << "jumlah kelopak : " <<bergerombol.jumlahkelopak<<endl<<endl;
+	
+	cout << (berbiji.jenisbunga);
+	berbiji.muda("Kuning") ;
+	berbiji.layu("kuning kecoklatan");
+	cout << "jumlah kelopak : " <<berbiji.jumlahkelopak<<endl<<endl;
+	
 	
 		
+}
+
+
+//warna layu
+void bunga::layu(string warna){
+	cout << "Warnanya jika Layu adalah "<<warna<< " !!"<<endl;	
 }
